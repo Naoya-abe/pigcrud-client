146ee5e16546c034b709fa46d086f9efc05d3016
阿部直也 <n.abe@gemcook.com>
2019-11-30 13:48:51 +0900

    [add] crud component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import ItemCreate from "./items/ItemCreate";
+import ItemDelete from "./items/ItemDelete";
+import ItemDetail from "./items/ItemDetail";
+import ItemEdit from "./items/ItemEdit";
+import ItemList from "./items/ItemList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={ItemList} />
+        <Route path="/items/new" component={ItemCreate} />
+        <Route path="/items/delete" component={ItemDelete} />
+        <Route path="/items/detail" component={ItemDetail} />
+        <Route path="/items/edit" component={ItemEdit} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
