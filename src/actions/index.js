72e9865a06dd726ee9acfa79ee52c10f5c76aa0d
阿部直也 <n.abe@gemcook.com>
2019-12-03 22:33:17 +0900

    [add] action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
+import { SIGN_IN, SIGN_OUT } from "./types";
+
 export const signIn = () => {
   return {
-    type: "SIGN_IN",
+    type: SIGN_IN,
     payload: true
   };
 };
 
 export const signOut = () => {
   return {
-    type: "SIGN_OUT",
+    type: SIGN_OUT,
     payload: false
   };
 };
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,12 +1,14 @@
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
+
 const INITIAL_STATE = {
   isSignedIn: null
 };
 
 export const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, isSignedIn: action.payload };
-    case "SIGN_OUT":
+    case SIGN_OUT:
       return { ...state, isSignedIn: action.payload };
     default:
       return state;
