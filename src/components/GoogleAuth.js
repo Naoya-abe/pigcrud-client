c09bfa4d25a31806678cbde05d1304daf1a4322a
阿部直也 <n.abe@gemcook.com>
2019-12-05 09:12:04 +0900

    [fix] comment position

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,3 +1,9 @@
+// index.htmlに<script></script>を書くと、gapiが使える。そこから、"load"を使って必要なAPIをダウンロードする。
+// gapi.load("client:auth2")と書くことで、clientモジュールとauth2モジュールを同時に取得できる。
+// callback関数を呼び出すと、promiseオブジェクトが返ってくる。
+// scopeはアクセストークンに付与するアクセス権の範囲を制限する手段
+// listenはloginの状態が変更されたら、その中のcallback関数にboolean(ログイン状態がtrue or false)を渡してcallback関数を実行する。
+
 import React from "react";
 import { connect } from "react-redux";
 import { signIn, signOut } from "../actions";
@@ -18,12 +24,6 @@ class GoogleAuth extends React.Component {
         });
     });
   }
-
-  // index.htmlに<script></script>を書くと、gapiが使える。そこから、"load"を使って必要なAPIをダウンロードする。
-  // gapi.load("client:auth2")と書くことで、clientモジュールとauth2モジュールを同時に取得できる。
-  // callback関数を呼び出すと、promiseオブジェクトが返ってくる。
-  // scopeはアクセストークンに付与するアクセス権の範囲を制限する手段
-  // listenはloginの状態が変更されたら、その中のcallback関数にboolean(ログイン状態がtrue or false)を渡してcallback関数を実行する。
 
   onAuthChange = isSignedIn => {
     if (isSignedIn) {
