3655fca15ba98c0d457c8011b5f1c5532ed67a97
阿部直也 <n.abe@gemcook.com>
2019-12-06 12:23:33 +0900

    [add] input field

--- src/components/items/ItemCreate.js
+++ src/components/items/ItemCreate.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const ItemCreate = () => {
-  return <div>ItemCreate</div>;
-};
+class ItemCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  }
 
-export default ItemCreate;
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({ form: "itemCreate" })(ItemCreate);
