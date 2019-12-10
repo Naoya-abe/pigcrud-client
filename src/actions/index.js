d0f78b9c017fe3e83d4cf09282beb84b4a3d9b40
阿部直也 <n.abe@gemcook.com>
2019-12-10 09:21:20 +0900

    [add] make action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import items from "../apis/items";
-import { SIGN_IN, SIGN_OUT, CREATE_ITEM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_ITEM,
+  FETCH_ITEMS,
+  FETCH_ITEM,
+  DELETE_ITEM,
+  EDIT_ITEM
+} from "./types";
 
 export const signIn = () => {
   return {
@@ -18,4 +26,24 @@ export const signOut = () => {
 export const createItems = formValues => async dispatch => {
   const response = await items.post("/items", formValues);
   dispatch({ type: CREATE_ITEM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await items.get("/items");
+  dispatch({ type: FETCH_ITEMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await items.get(`/items/${id}`);
+  dispatch({ type: FETCH_ITEM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await items.put(`/items/${id}`, formValues);
+  dispatch({ type: EDIT_ITEM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await items.delete(`/items/${id}`);
+  dispatch({ type: DELETE_ITEM, payload: id });
 };
